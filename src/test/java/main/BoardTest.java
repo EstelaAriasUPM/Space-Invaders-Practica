b16611ce1cca6d919da57a2e7ccfcf3ec4aa9e67
Javier Ferreño <javifc67@gmail.com>
2024-10-28 20:04:09 +0100

    añadir proyecto

---
+++ src/test/java/main/BoardTest.java
@@ -0,0 +1,18 @@
+package main;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class BoardTest {
+	Board tablero;
+	
+	@BeforeEach
+	public void setUp() {
+		tablero = new Board();
+	}
+	
+	public void updateInit() {
+		
+		
+	}
+	
+}

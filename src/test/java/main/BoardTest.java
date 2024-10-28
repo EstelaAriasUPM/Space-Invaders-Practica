c5ef98e36c33884747942d3e94e0ea80d382a7eb
Javier Ferreño <javifc67@gmail.com>
2024-10-28 21:02:01 +0100

    añadir test board base

--- src/test/java/main/BoardTest.java
+++ src/test/java/main/BoardTest.java
@@ -1,6 +1,9 @@
 package main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 	Board tablero;
@@ -10,8 +13,28 @@ public class BoardTest {
 		tablero = new Board();
 	}
 	
-	public void updateInit() {
+	@Test
+	void update_bomb() {
 		
+	}	
+	
+	@Test
+	void update_aliens() {
+		
+	}
+	
+	@Test
+	void update_shoots() {
+		
+	}
+	
+	@Test
+	void update() {
+		
+	}
+	
+	@Test
+	void gameInit() {
 		
 	}
 	

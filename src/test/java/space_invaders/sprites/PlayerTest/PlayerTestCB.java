eb4703c9d3d0ad5be164a1478c608b0b04588d29
GuillermoSparrowe <guillermosparrowe02@gmail.com>
2024-11-18 20:47:21 +0100

    modificaciones de player y alien para que tengan relación con la documentación

--- src/test/java/space_invaders/sprites/PlayerTest/PlayerTestCB.java
+++ src/test/java/space_invaders/sprites/PlayerTest/PlayerTestCB.java
@@ -21,30 +21,39 @@ public class PlayerTestCB {
 	
 	   @Test
 	    @DisplayName("comprueba act")
-	    public void testPlayerAct() {
-	        player.setX(400);
-	        player.setY(400);
-	        assertEquals(Commons.BOARD_WIDTH - player.getWidth(), player.getX());
-	        assertEquals(Commons.BOARD_HEIGHT, player.getY());
-	        assertNotNull(player.getImage());
+	    public void testPlayerAct() {  //CP2 no se puede probar
+		   //CP3
+		   player.setX(0);
+		   player.setdx(-2);
+		   player.act();
+
+		   // CP1
+		   player.setX(Commons.BOARD_WIDTH);
+		   player.setdx(2);
+		   player.act();
+
 	    }
 
 		@Test
 		@DisplayName("comprueba keypressed")
 		public void testPlayerKeyPressed() {
 		KeyEvent keyLeft = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
+		KeyEvent keySpace = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_SPACE, KeyEvent.CHAR_UNDEFINED);
 		KeyEvent keyRight = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
-		player.keyPressed(keyLeft);
-		player.keyPressed(keyRight);
+		player.keyPressed(keyLeft); //CP1
+		 player.keyPressed(keySpace); //CP2
+		player.keyPressed(keyRight); //CP3
 	}
 
 		@Test
 		@DisplayName("comprueba keyreleased")
 		public void testPlayerKeyReleased() {
 		KeyEvent keyLeft = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
-		KeyEvent keyRight = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
-		player.keyReleased(keyLeft);
-		player.keyReleased(keyRight);
+		KeyEvent keySpace = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_SPACE, KeyEvent.CHAR_UNDEFINED);
+			KeyEvent keyRight = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
+		player.keyReleased(keyLeft); //CP1
+		player.keyReleased(keySpace); //CP2
+		player.keyReleased(keyRight); //CP3
 		}
 	}
 

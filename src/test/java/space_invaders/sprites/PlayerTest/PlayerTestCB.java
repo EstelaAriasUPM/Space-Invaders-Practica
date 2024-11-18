edb9fdc4827ffd9a087736c94f1e4c3a64873ae3
Javier Ferreño <javifc67@gmail.com>
2024-11-18 18:24:25 +0100

    todos los test caja blanca

--- src/test/java/space_invaders/sprites/PlayerTest/PlayerTestCB.java
+++ src/test/java/space_invaders/sprites/PlayerTest/PlayerTestCB.java
@@ -14,37 +14,45 @@ import java.awt.event.KeyEvent;
 
 public class PlayerTestCB {
 	Player player;
+
 	@BeforeEach
 	public void init() {
 		player = new Player();
 	}
-	
-	   @Test
-	    @DisplayName("comprueba act")
-	    public void testPlayerAct() {
-	        player.setX(400);
-	        player.setY(400);
-	        assertEquals(Commons.BOARD_WIDTH - player.getWidth(), player.getX());
-	        assertEquals(Commons.BOARD_HEIGHT, player.getY());
-	        assertNotNull(player.getImage());
-	    }
 
-		@Test
-		@DisplayName("comprueba keypressed")
-		public void testPlayerKeyPressed() {
-		KeyEvent keyLeft = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
-		KeyEvent keyRight = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
+	@Test
+	@DisplayName("comprueba act")
+	public void testPlayerAct() {
+		// Límite izquierdo
+		player.setX(0);
+		player.setdx(-2);
+		player.act();
+
+		// Límite derecho NO SE PUEDE PROBAR DEBIDO A EL METODO ACT
+		player.setX(Commons.BOARD_WIDTH);
+		player.setdx(2);
+		player.act();
+	}
+
+	@Test
+	@DisplayName("comprueba keypressed")
+	public void testPlayerKeyPressed() {
+		KeyEvent keyLeft = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,
+				KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
+		KeyEvent keyRight = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,
+				KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
 		player.keyPressed(keyLeft);
 		player.keyPressed(keyRight);
 	}
 
-		@Test
-		@DisplayName("comprueba keyreleased")
-		public void testPlayerKeyReleased() {
-		KeyEvent keyLeft = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
-		KeyEvent keyRight = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
+	@Test
+	@DisplayName("comprueba keyreleased")
+	public void testPlayerKeyReleased() {
+		KeyEvent keyLeft = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,
+				KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
+		KeyEvent keyRight = new KeyEvent(new java.awt.Button(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,
+				KeyEvent.VK_RIGHT, KeyEvent.CHAR_UNDEFINED);
 		player.keyReleased(keyLeft);
 		player.keyReleased(keyRight);
-		}
 	}
-
+}

890fbb8a37ac4bdb4afb63883924e6c5c373fa8d
Javifc <javifc67@gmail.com>
2024-11-27 19:41:30 +0100

    corregidos los errores de CN de shot

--- src/main/java/space_invaders/sprites/Shot.java
+++ src/main/java/space_invaders/sprites/Shot.java
@@ -2,37 +2,56 @@ package space_invaders.sprites;
 
 import javax.swing.ImageIcon;
 
+import main.Commons;
+
 public class Shot extends Sprite {
 
-    public Shot() {
-    }
-    /**
-     * Inicializa un nuevo objeto disparo en las coordenadas indicadas
-     * @param x coordenada X de la posición del nuevo disparo
-     * @param y coordenada Y de la posición del nuevo disparo
-     * */
-    public Shot(int x, int y) {
+	public Shot() {
+	}
 
-        initShot(x, y);
-    }
-    /**
-     * Inicializa un nuevo objeto disparo en las coordenadas indicadas y le asigna la imagen correspondiente en la interfaz
-     * @param x coordenada X de la posición del nuevo disparo
-     * @param y coordenada Y de la posición del nuevo disparo
-     * Si alguna de las coordenadas indicadas es mayor al máximo permitido, se le asignará el valor máximo permitido.
-     * Si se introducen valores negativos de coordenada, se asignará el mínimo permitido (0).
-     * */
-    private void initShot(int x, int y) {
+	/**
+	 * Inicializa un nuevo objeto disparo en las coordenadas indicadas
+	 * 
+	 * @param x coordenada X de la posición del nuevo disparo
+	 * @param y coordenada Y de la posición del nuevo disparo
+	 */
+	public Shot(int x, int y) {
 
-        var shotImg = "src/main/resources/images/shot.png";
-        var ii = new ImageIcon(shotImg);
-        setImage(ii.getImage());
+		initShot(x, y);
+	}
 
-        int H_SPACE = 6;
-        setX(x + H_SPACE);
+	/**
+	 * Inicializa un nuevo objeto disparo en las coordenadas indicadas y le asigna
+	 * la imagen correspondiente en la interfaz
+	 * 
+	 * @param x coordenada X de la posición del nuevo disparo
+	 * @param y coordenada Y de la posición del nuevo disparo Si alguna de las
+	 *          coordenadas indicadas es mayor al máximo permitido, se le asignará
+	 *          el valor máximo permitido. Si se introducen valores negativos de
+	 *          coordenada, se asignará el mínimo permitido (0).
+	 */
+	private void initShot(int x, int y) {
 
-        int V_SPACE = 1;
-        setY(y - V_SPACE);
-    }
+		var shotImg = "src/main/resources/images/shot.png";
+		var ii = new ImageIcon(shotImg);
+		setImage(ii.getImage());
+		int H_SPACE = 6;
+		int V_SPACE = 1;
+
+		// Arreglo de bug(iniciar shot en valores por fuera de tablero)
+		if (x < 0) {
+			x = 0;
+		} else if (x > Commons.BOARD_WIDTH) {
+			x = Commons.BOARD_WIDTH - H_SPACE;
+		}
+		if (y < 0) {
+			y = V_SPACE;
+		} else if (y > Commons.BOARD_HEIGHT) {
+			y = Commons.BOARD_HEIGHT;
+		}
+
+		setX(x + H_SPACE);
+
+		setY(y - V_SPACE);
+	}
 }
-

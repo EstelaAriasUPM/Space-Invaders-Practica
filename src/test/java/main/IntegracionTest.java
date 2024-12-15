d749835ee961bdc49620340515f9495556fe0551
robotcgb <cgbarrios.02@gmail.com>
2024-12-15 19:04:46 +0100

    Ahora el pom va bien

--- src/test/java/main/IntegracionTest.java
+++ src/test/java/main/IntegracionTest.java
@@ -3,18 +3,26 @@ package main;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import space_invaders.sprites.Alien;
+import space_invaders.sprites.*;
 
 import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
 //import org.mockito.junit.jupiter.MockitoExtension;
 //import org.mockito.junit.jupiter.MockitoSettings;
 
 public class IntegracionTest {
 
     @Mock
-    private ArrayList<Alien> aliens = new ArrayList<>();
+    private Alien alienMock;
 
     @Test
-
+    public void testInitBomb(){
+        when(alienMock.getX()).thenReturn(100);
+        when(alienMock.getY()).thenReturn(100);
+        
+        Alien.Bomb bomb = alienMock.getBomb();
+        
+    }
 
 }

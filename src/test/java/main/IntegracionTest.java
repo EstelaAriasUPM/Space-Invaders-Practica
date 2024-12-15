f4ae7e4a26e84f477499f8ade7ba89e20833f8bf
robotcgb <cgbarrios.02@gmail.com>
2024-12-15 18:55:16 +0100

    Cambiando cosas para meter la parte de Mockito

---
+++ src/test/java/main/IntegracionTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import space_invaders.sprites.Alien;
+
+import java.util.ArrayList;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.mockito.junit.jupiter.MockitoSettings;
+
+public class IntegracionTest {
+
+    @Mock
+    private ArrayList<Alien> aliens = new ArrayList<>();
+
+    @Test
+
+
+}
